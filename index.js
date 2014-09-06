a29eb25f2e84b43b168e0741fb589aaeb72bc181
jonschlinkert <dev@sellside.com>
2014-09-06 03:36:58 -0400

    adds tests
    ensure that file exists

--- index.js
+++ index.js
@@ -1,28 +1,38 @@
+/*!
+ * is-directory <https://github.com/jonschlinkert/is-directory>
+ *
+ * Copyright (c) 2014 Jon Schlinkert, contributors.
+ * Licensed under the MIT License
+ */
 
 'use strict';
 
 var fs = require('fs');
 
-var isDir = function isDir(filepath, callback) {
-  if(typeof callback !== 'function') {
+var isDir = function isDir(filepath, cb) {
+  if (typeof cb !== 'function') {
     return isDir.sync(filepath);
   }
 
-  fs.stat(filepath, function(err, stats){
-    if(err) {
-      callback(err);
+  fs.stat(filepath, function (err, stats) {
+    if (err) {
+      cb(err);
       return;
     }
 
-    callback(null, stats.isDirectory());
+    cb(null, stats.isDirectory());
     return;
   });
 };
 
 
 isDir.sync = function isDirSync(filepath) {
+  if (!fs.existsSync(filepath)) {
+    return false;
+  }
+
   var stat = fs.statSync(filepath);
-  return stat && stat.isDirectory();
+  return stat.isDirectory();
 };
 
 module.exports = isDir;
--- test.js
+++ test.js
@@ -7,14 +7,56 @@
 
 'use strict';
 
+var should = require('should');
 var assert = require('assert');
 var isDir = require('./');
 
-function isFile(filepath) {
-  return !isDir(filepath);
+function isFile(filepath, cb) {
+  return !isDir(filepath, cb);
 }
 
 describe('isDir', function () {
+  it('should return `true` if the path is a directory', function (done) {
+    isDir('node_modules', function (err, dir) {
+      if (err) console.log(err);
+      dir.should.be.true;
+    });
+    !isDir('.jshintrc', function (err, dir) {
+      if (err) console.log(err);
+      dir.should.be.false;
+    });
+    !isDir('README.md', function (err, dir) {
+      if (err) console.log(err);
+      dir.should.be.false;
+    });
+    !isDir('LICENSE-MIT', function (err, dir) {
+      if (err) console.log(err);
+      dir.should.be.false;
+    });
+
+    isFile('.jshintrc', function (err, dir) {
+      if (err) console.log(err);
+      dir.should.be.false;
+    });
+    isFile('README.md', function (err, dir) {
+      if (err) console.log(err);
+      dir.should.be.false;
+    });
+    isFile('LICENSE-MIT', function (err, dir) {
+      if (err) console.log(err);
+      dir.should.be.false;
+    });
+    done();
+  });
+
+  it('should return `false` if the path is not a directory', function () {
+    assert.equal(isDir('.jshintrc'), false);
+    assert.equal(isDir('README.md'), false);
+    assert.equal(isDir('LICENSE-MIT'), false);
+  });
+});
+
+describe('isDir sync', function () {
   it('should return `true` if the path is a directory', function () {
     assert(isDir('node_modules'));
     assert(!isDir('.jshintrc'));
@@ -30,5 +72,18 @@ describe('isDir', function () {
     assert.equal(isDir('.jshintrc'), false);
     assert.equal(isDir('README.md'), false);
     assert.equal(isDir('LICENSE-MIT'), false);
+  });
+
+  it('should return `true` if the path is a directory', function () {
+    assert(isDir.sync('node_modules'));
+    assert(!isDir.sync('.jshintrc'));
+    assert(!isDir.sync('README.md'));
+    assert(!isDir.sync('LICENSE-MIT'));
+  });
+
+  it('should return `false` if the path is not a directory', function () {
+    assert.equal(isDir.sync('.jshintrc'), false);
+    assert.equal(isDir.sync('README.md'), false);
+    assert.equal(isDir.sync('LICENSE-MIT'), false);
   });
 });
