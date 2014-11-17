86d67ae5aaa9d25878ae9821bb33cabb024cc49f
jonschlinkert <dev@sellside.com>
2014-11-17 02:39:48 -0500

    lint, run verb

--- test.js
+++ test.js
@@ -21,15 +21,15 @@ describe('isDir', function () {
       if (err) console.log(err);
       dir.should.be.true;
     });
-    !isDir('.jshintrc', function (err, dir) {
+    isDir('.jshintrc', function (err, dir) {
       if (err) console.log(err);
       dir.should.be.false;
     });
-    !isDir('README.md', function (err, dir) {
+    isDir('README.md', function (err, dir) {
       if (err) console.log(err);
       dir.should.be.false;
     });
-    !isDir('LICENSE-MIT', function (err, dir) {
+    isDir('LICENSE-MIT', function (err, dir) {
       if (err) console.log(err);
       dir.should.be.false;
     });
@@ -50,40 +50,40 @@ describe('isDir', function () {
   });
 
   it('should return `false` if the path is not a directory', function () {
-    assert.equal(isDir('.jshintrc'), false);
-    assert.equal(isDir('README.md'), false);
-    assert.equal(isDir('LICENSE-MIT'), false);
+    isDir('.jshintrc').should.be.false;
+    isDir('README.md').should.be.false;
+    isDir('LICENSE-MIT').should.be.false;
   });
 });
 
 describe('isDir sync', function () {
   it('should return `true` if the path is a directory', function () {
-    assert(isDir('node_modules'));
+    isDir('node_modules').should.be.true;
     assert(!isDir('.jshintrc'));
     assert(!isDir('README.md'));
     assert(!isDir('LICENSE-MIT'));
 
-    assert(isFile('.jshintrc'));
-    assert(isFile('README.md'));
-    assert(isFile('LICENSE-MIT'));
+    isFile('.jshintrc').should.be.true;
+    isFile('README.md').should.be.true;
+    isFile('LICENSE-MIT').should.be.true;
   });
 
   it('should return `false` if the path is not a directory', function () {
-    assert.equal(isDir('.jshintrc'), false);
-    assert.equal(isDir('README.md'), false);
-    assert.equal(isDir('LICENSE-MIT'), false);
+    isDir('.jshintrc').should.be.false;
+    isDir('README.md').should.be.false;
+    isDir('LICENSE-MIT').should.be.false;
   });
 
   it('should return `true` if the path is a directory', function () {
-    assert(isDir.sync('node_modules'));
+    isDir.sync('node_modules').should.be.true;
     assert(!isDir.sync('.jshintrc'));
     assert(!isDir.sync('README.md'));
     assert(!isDir.sync('LICENSE-MIT'));
   });
 
   it('should return `false` if the path is not a directory', function () {
-    assert.equal(isDir.sync('.jshintrc'), false);
-    assert.equal(isDir.sync('README.md'), false);
-    assert.equal(isDir.sync('LICENSE-MIT'), false);
+    isDir.sync('.jshintrc').should.be.false;
+    isDir.sync('README.md').should.be.false;
+    isDir.sync('LICENSE-MIT').should.be.false;
   });
 });
