47cdb9196be1efb5bf03f0a4453fd00d7fc1c9aa
jonschlinkert <jon.schlinkert@sellside.com>
2015-03-21 21:55:55 -0400

    lint

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /*!
  * is-directory <https://github.com/jonschlinkert/is-directory>
  *
- * Copyright (c) 2014 Jon Schlinkert, contributors.
- * Licensed under the MIT License
+ * Copyright (c) 2014-2015, Jon Schlinkert.
+ * Licensed under the MIT License.
  */
 
 'use strict';
